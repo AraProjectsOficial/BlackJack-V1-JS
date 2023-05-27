c992bff90d79e47a6d52f2462e1734d6e6c2e900
RafaEscobar <Rafael.EG.code@gmail.com>
2023-05-27 17:18:05 -0600

    Fin

--- js/app.js
+++ js/app.js
@@ -8,25 +8,34 @@ let letras = ['C', 'D', 'H', 'S'];
 let letrasEs = ['A', 'J', 'K', 'Q'];
 
 //* Contadores de puntaje
-let puntajeJ=0, puntajeC=0;
+let puntajeJ=0, puntajeC=0, turnoJ=0, turnoC = false;
 
 //* Referencias a btns
 reiniciar = document.querySelector('#reiniciar');
 pedir = document.querySelector('#pedir');
 detener = document.querySelector('#detener');
+detener.disabled = true;
 
 //* Referencias a puntaje y turno
 let boxPuntajeJ = document.querySelector('#puntajeJ');
+let boxPuntajeC = document.querySelector('#puntajeC');
 let boxTurnoJ = document.querySelector('#turnoJ');
 
 //* Referencias a cajas
-boxJugador = document.querySelector('#cajaJugador');
-boxComputadora = document.querySelector('#cajaComputadora')
+let boxJugador = document.querySelector('#cajaJugador');
+let boxComputadora = document.querySelector('#cajaComputadora');
 
 //* Construir baraja
 const generarBaraja = () => {
-    //! Vaceamos array de abraja 
+    //! Vaceamos array de abraja, puntajes y turno; lo insertamos
     baraja = [];
+
+    turnoC = false;
+
+    boxPuntajeJ.innerText = puntajeJ=0;
+    boxPuntajeC.innerText = puntajeC=0;
+    boxTurnoJ.innerText = turnoJ=0;
+
     //! Parte 1 de la baraja
     for(let i=2; i<=10; i++){
         for(let mensajero of letras){
@@ -51,23 +60,62 @@ const barajear = () => {
 
 barajear();
 
-//* Pedir carta
-pedir.addEventListener('click', () => {
+const obtenerValor = () => {
     //! Obtenemos carta
+    detener.disabled = false;
     let carta = baraja.pop();
+
     //! Obtenemos valor de carta
-    let valor = carta.charAt(0);
+    let valor = carta.slice(0, -1);
     (!isNaN(valor)) ? valor = parseInt(valor) : 
     (['J', 'K', 'Q'].includes(valor)) ? valor = 10 : valor = 11;
-    //! Acumulamos y mostrar puntaje
-    puntajeJ += valor;
-    boxPuntajeJ.innerText = puntajeJ;
 
-    console.log(valor);
+    if (turnoC) {
+        //! Acumulamos y mostrar puntaje y turno
+        puntajeC += valor;
+        boxPuntajeC.innerText = puntajeC;
+    }else{
+        //! Acumulamos y mostrar puntaje y turno
+        puntajeJ += valor;
+        boxPuntajeJ.innerText = puntajeJ;
+        boxTurnoJ.innerText = (++turnoJ);
+    }
+    genCarta(carta);
+}
+
+const genCarta = (carta) => {
+    //! Creamos elemento html -img-
     let img = document.createElement('img');
+    //! Le asignamos la ruta dinamicamente en base a la carta obtenida
     img.src = `./assets/${carta}.png`;
+    //! Le agregamos la clase css correspondiente
     img.classList.add('cajaImagen');
-    boxJugador.append(img);
+    //! Insertamos dentro del elemento referencia -boxJugador-, el elemento imagen con la ruta y clase dada
+    (turnoC==true) ? boxComputadora.append(img) : boxJugador.append(img);
+}
+
+const revisar = () => {
+    //! GANADOR
+    if(puntajeJ==21){
+        (puntajeC==21) ? alert('Empate') : 
+        (puntajeC<21 || puntajeC>21) ? alert('Ganaste!!') : null; 
+        bloquear(true);
+    }else if(puntajeJ<21 && puntajeC!=0){
+        (puntajeC==21) ? alert('Gana la IA') :
+        (puntajeC>21) ? alert('Ganaste!!!') : 
+        (puntajeC<21) ? alert('Empate') : null;
+    }else if(puntajeJ>21){
+        setTimeout(function() {
+            alert('Gano la IA');
+          }, 100);
+        generarTurnoPC();
+    }
+}
+
+//* Pedir carta
+pedir.addEventListener('click', () => {
+    obtenerValor();
+    revisar();
 });
 
 //* Reiniciar
@@ -75,5 +123,25 @@ reiniciar.addEventListener('click', () => {
     generarBaraja();
     barajear();
     boxJugador.innerText = null;
-
+    boxComputadora.innerText = null;
+    bloquear(false);
 });
+
+detener.addEventListener('click', () => {
+    generarTurnoPC();
+    revisar();
+});
+
+const generarTurnoPC = () => {
+    turnoC = true;
+    do{
+        obtenerValor();
+        if(puntajeJ>21){break;}
+    }while(puntajeC<=puntajeJ);
+    bloquear(true);
+}
+
+const bloquear = (val, val2 = true) => {
+    pedir.disabled = val;
+    detener.disabled = val2;
+}
